1861e401d8f4c260e21a31fdcbaef10b8c4fa56b
unknown <ctaicarusgmail.com>
2022-04-25 08:46:23 +0700

    data

--- ecommerce_test/ecom/models.py
+++ ecommerce_test/ecom/models.py
@@ -16,33 +16,28 @@ class Customer(models.Model):
         return self.user.first_name
 
 
-class Product(models.Model):
-    name=models.CharField(max_length=40)
-    product_image= models.ImageField(upload_to='product_image/',null=True,blank=True)
-    price = models.PositiveIntegerField()
-    description=models.CharField(max_length=40)
-    def __str__(self):
-        return self.name
-
 
 class Cake(models.Model):
     name=models.CharField(max_length=40)
-    product_image= models.ImageField(upload_to='product_image/',null=True,blank=True)
-    price = models.PositiveIntegerField()
-    description=models.CharField(max_length=40)
     TYPE =(
         ('Bánh sinh nhật','Bánh sinh nhật'),
         ('Bánh mặn','Bánh mặn'),
         ('Bánh ngọt','Bánh ngọ'),
     )
     type = models.CharField(max_length=200, null = True, choices = TYPE)
+    expiry = models.CharField(max_length=40, null = True)
     def __str__(self):
         return self.name
 
+class Cakeitem(models.Model):
+    product_image= models.ImageField(upload_to='product_image/',null=True,blank=True)
+    cake = models.ForeignKey('Cake', on_delete=models.CASCADE,null=True)
+    discount=models.PositiveIntegerField()
+    price=models.FloatField()
+    description=models.CharField(max_length=40)
+
 class Drink(models.Model):
     name=models.CharField(max_length=40)
-    product_image= models.ImageField(upload_to='product_image/',null=True,blank=True)
-    price = models.PositiveIntegerField()
     description=models.CharField(max_length=40)
     SIZE =(
         ('L','L'),
@@ -50,31 +45,76 @@ class Drink(models.Model):
         ('XL','XL'),
     )
     size = models.CharField(max_length=200, null = True, choices = SIZE)
+    TYPE =(
+        ('Giải khát','Giải khát'),
+        ('Nước ngọt','Nước ngọt'),
+        ('Trà sữa','Trà sữa'),
+    )
+    type = models.CharField(max_length=200, null = True, choices = TYPE)    
+    expiry = models.CharField(max_length=40, null = True)
     def __str__(self):
         return self.name
 
+class Drinkitem(models.Model):
+    product_image= models.ImageField(upload_to='product_image/',null=True,blank=True)
+    drink = models.ForeignKey('Drink', on_delete=models.CASCADE,null=True)    
+    discount=models.PositiveIntegerField()
+    price=models.FloatField()
+    description=models.CharField(max_length=40)    
+
+class Cart(models.Model):
+    cakeitem=models.ForeignKey('Cakeitem',on_delete=models.CASCADE,null=True)
+    drinkitem=models.ForeignKey('Drinkitem',on_delete=models.CASCADE,null=True)
+    dateCreated= models.DateField(auto_now_add=True,null=True)
+    totalMoney=models.FloatField()
+
+class Paypal(models.Model):
+    idPaypal = models.PositiveIntegerField()
+    datePay = models.DateField(auto_now_add=True,null=True)
+
+class Shipment(models.Model):
+    name = models.CharField(max_length=40)
+    price = models.FloatField()
+    
+class Payment(models.Model):
+    totalMoney = models.FloatField()
+    paypal = models.ForeignKey('Paypal',on_delete=models.CASCADE,null=True)
+    shipment = models.ForeignKey('Shipment',on_delete=models.CASCADE,null=True)
+
 
 class Orders(models.Model):
+    customer=models.ForeignKey('Customer', on_delete=models.CASCADE,null=True)
+    cart=models.ForeignKey('Cart', on_delete=models.CASCADE,null=True)
+    payment = models.ForeignKey('Payment', on_delete=models.CASCADE,null=True)
     STATUS =(
         ('Pending','Pending'),
         ('Order Confirmed','Order Confirmed'),
         ('Out for Delivery','Out for Delivery'),
         ('Delivered','Delivered'),
-    )
-    customer=models.ForeignKey('Customer', on_delete=models.CASCADE,null=True)
-    product=models.ForeignKey('Product',on_delete=models.CASCADE,null=True)
-    cake=models.ForeignKey('Cake',on_delete=models.CASCADE,null=True)
-    drink=models.ForeignKey('Drink',on_delete=models.CASCADE,null=True)
-    email = models.CharField(max_length=50,null=True)
-    address = models.CharField(max_length=500,null=True)
-    mobile = models.CharField(max_length=20,null=True)
-    order_date= models.DateField(auto_now_add=True,null=True)
+    )    
     status=models.CharField(max_length=50,null=True,choices=STATUS)
+    dateCreated= models.DateField(auto_now_add=True,null=True)
 
 
 class Feedback(models.Model):
     name=models.CharField(max_length=40)
     feedback=models.CharField(max_length=500)
     date= models.DateField(auto_now_add=True,null=True)
+    def __str__(self):
+        return self.name
+
+    
+class Comment(models.Model):
+    content = models.CharField(max_length=500)
+    customer = models.ForeignKey('Customer', on_delete=models.CASCADE,null=True)
+    cakeitem=models.ForeignKey('Cakeitem',on_delete=models.CASCADE,null=True)
+    drinkitem=models.ForeignKey('Drinkitem',on_delete=models.CASCADE,null=True)
+  
+# tao form    
+class Product(models.Model):
+    name=models.CharField(max_length=40)
+    product_image= models.ImageField(upload_to='product_image/',null=True,blank=True)
+    price = models.PositiveIntegerField()
+    description=models.CharField(max_length=40)
     def __str__(self):
         return self.name
