c78050e5bf7c75fb7c973fce339fa8cc710c5e8e
trickstarcandina <rinnegan101220@gmail.com>
2022-04-30 23:31:31 +0700

    feat: init shippment

--- ecommerce_test/ecom/forms.py
+++ ecommerce_test/ecom/forms.py
@@ -43,3 +43,9 @@ class ContactusForm(forms.Form):
     Name = forms.CharField(max_length=30)
     Email = forms.EmailField()
     Message = forms.CharField(max_length=500,widget=forms.Textarea(attrs={'rows': 3, 'cols': 30}))
+
+#for shippment
+class ShipmentForm(forms.ModelForm):
+    class Meta:
+        model=models.Shipment
+        fields=['name','price']
